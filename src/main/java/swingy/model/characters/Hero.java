25a8fc104c3c45386093a3bd01a42f5e6523c1d4
Zain <zmathews@student.wethinkcode.co.za>
2020-09-03 01:53:40 +0200

    changes nothing in pom, pom stops complaining

--- src/main/java/swingy/model/characters/Hero.java
+++ src/main/java/swingy/model/characters/Hero.java
@@ -1,6 +1,6 @@
 package src.main.java.swingy.model.characters;
 
-public class Hero extends BaseHero{
+public class Hero extends BaseHero {
 	public Hero(String heroName, String heroClass) {
 		super(heroName, heroClass);
 	}
