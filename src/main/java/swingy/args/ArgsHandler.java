1f0e47e4106d7270d5958fd7a1e4981517113373
Zain <zmathews@student.wethinkcode.co.za>
2020-09-03 03:01:54 +0200

    added ignore target build test files to gitignore, built basic window with swing, can't test in console based os oops

--- src/main/java/swingy/args/ArgsHandler.java
+++ src/main/java/swingy/args/ArgsHandler.java
@@ -1,5 +1,7 @@
 package swingy.args;
 
+import javax.swing.*;
+
 import swingy.exception.StandardException;
 
 public class ArgsHandler {
@@ -9,9 +11,16 @@ public class ArgsHandler {
 			if (args.length != 1) {
 				throw new StandardException("Invalid number of arguments passed");
 			}
-			if (args[0].toLowerCase() != "console" && args[0].toLowerCase() != "gui") {
+			if (!args[0].toLowerCase().equals("console") && !args[0].toLowerCase().equals("gui")) {
 				throw new StandardException(args[0] + " is not a valid argument");
 			}
+			JFrame jFrame = new JFrame();
+			JButton jButton = new JButton();
+			jButton.setBounds(130, 100, 100, 50); //x, y, width, height
+			jFrame.add(jButton);
+			jFrame.setSize(400, 500);
+			jFrame.setLayout(null); //no layout managers
+			jFrame.setVisible(true);
 		}
 		catch (StandardException e){
 			System.out.println(e.getMessage());
