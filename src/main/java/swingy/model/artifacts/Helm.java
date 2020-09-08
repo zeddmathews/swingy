277ab201d7fe15a9c0483eb115811fb5900128c6
Zain <zmathews@student.wethinkcode.co.za>
2020-09-08 15:19:59 +0200

    this is fine on wsl

--- src/main/java/swingy/model/artifacts/Helm.java
+++ src/main/java/swingy/model/artifacts/Helm.java
@@ -5,6 +5,7 @@ import swingy.controller.ArtifactController;
 public class Helm extends Artifacts implements ArtifactController {
 	public Helm(String type, String affectedStat, int stats, int heroLevel) {
 		super(type, affectedStat, stats, heroLevel);
+		this.type = "helm";
 	}
 
 	public int generateStats(String affectedStat, int heroLevel) {
--- src/main/java/swingy/util/ArtifactCreator.java
+++ src/main/java/swingy/util/ArtifactCreator.java
@@ -1,5 +1,8 @@
 package swingy.util;
 
+import swingy.model.artifacts.Helm;
+import swingy.controller.ArtifactController;
+
 public abstract class ArtifactCreator {
 	public static ArtifactController newArtifact(String type, String affectedStat, int stats, int heroLevel) {
 		return new Helm(type, affectedStat, stats, heroLevel);
